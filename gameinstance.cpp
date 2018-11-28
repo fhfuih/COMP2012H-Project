a6abc84c37efe9bc68ce293c8604269295d96365
William <william_yam@outlook.com>
2018-11-29 03:42:35 +0800

    Implementation of combat game interactions

--- gameinstance.cpp
+++ gameinstance.cpp
@@ -2,19 +2,18 @@
 
 GameInstance::GameInstance(int level, int PetMonsterID[5], QWidget *parent) {
     orb_game = new OrbGame();
-    //combat_game = new CombatGame(level, PetMonsterID);
+    combat_game = new CombatGame(level, PetMonsterID);
 
     orb_window = new OrbGameWindow(orb_game->orbBoard);
     orb_window->show();
 
     vector<int> monster {fileLoader().getLevel(level)};
     int EnemyMonsterID[5] {monster[0], monster[1], monster[2], monster[3], monster[4]};
-    //combat_window = new CombatGameWindow(PetMonsterID, EnemyMonsterID);
+    combat_window = new CombatGameWindow(PetMonsterID, EnemyMonsterID);
+    combat_window->show();
 
-    /*
     //Orb game and combat game
     connect(orb_game, &OrbGame::combo_finish, combat_game, &CombatGame::start_combat);
-    */
 
     //Orb game processor with UI
     connect(orb_window, &OrbGameWindow::orb_selected, orb_game, &OrbGame::on_orb_select);
@@ -22,7 +21,6 @@ GameInstance::GameInstance(int level, int PetMonsterID[5], QWidget *parent) {
     connect(orb_window, &OrbGameWindow::orb_deselected, orb_game, &OrbGame::on_finish_move);
     connect(orb_game, &OrbGame::refresh_board, orb_window, &OrbGameWindow::refresh_board);
 
-    /*
     //Combat game processor with UI
     connect(combat_window, &CombatGameWindow::SelectedPetMonster, combat_game, &CombatGame::activate_pet_ability);
     connect(combat_game, &CombatGame::pet_attack_enemy, combat_window, &CombatGameWindow::PetAttackEnemy);
@@ -32,17 +30,9 @@ GameInstance::GameInstance(int level, int PetMonsterID[5], QWidget *parent) {
     connect(combat_game, &CombatGame::special_attack_ready, combat_window, &CombatGameWindow::PetSkillReady);
     connect(combat_game, &CombatGame::enemy_die, combat_window, &CombatGameWindow::EnemyDie);
     connect(combat_game, &CombatGame::player_die, combat_window, &CombatGameWindow::PlayerDie);
-    */
+    connect(combat_game, &CombatGame::level_cleared, combat_window, &CombatGameWindow::LevelCleared);
 
     /* Other signals and slots
-    OrbGame
-        signals:
-            void refresh_board(const vector<BoardState>& statesVector);
-
-    OrbGameWindow
-        public slots:
-            //need to make a slot to refresh_board()
-
     CombatGame
 
     CombatGameWindow
