105882b3c93bd349678fbc954fdd9bf6e5b84f4c
William <william_yam@outlook.com>
2018-11-29 06:07:12 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,27 +16,9 @@ int main(int argc, char *argv[])
     m.show();
 #endif
 
-#ifdef RUN_GAME_INSTANCE
-    int level = 1;
-    int PetMonsterID[5] {101, 102, 103, 104,105};
-    GameInstance g(level, PetMonsterID);
-#endif
-
-#ifdef RUN_ABOUT_WINDOW
-    AboutWindow about;
-    about.show();
-#endif
-
 #ifdef RUN_START_WINDOW
     StartWindow start;
     start.show();
-#endif
-
-#ifdef RUN_COMBAT_GAME_WINDOW
-    int PetMonsterID[5] = {101, 102, 103, 104, 105};
-    int EnemyMonsterID[5] = {201, 202, 203, 204, 205};
-    CombatGameWindow w(PetMonsterID, EnemyMonsterID);
-    w.show();
 #endif
 
     return a.exec();
