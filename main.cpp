7e72d5ad4ee2c3ab910bc557251a411069150a8e
fhfuih <sam.zyhuang@outlook.com>
2018-11-30 18:38:54 +0800

    Add font file to resource

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "gameinstance.h"
 #include "AboutWindow.h"
 #include "StartWindow.h"
+#include <QFontDatabase>
 #include <QApplication>
 #include <cstdlib>
 #include <ctime>
@@ -10,7 +11,7 @@ int main(int argc, char *argv[])
 {
     srand(time(nullptr));
     QApplication a(argc, argv);
-
+    QFontDatabase::addApplicationFont(":/resource/Long_Shot.ttf");
 #ifdef RUN_MAIN_WINDOW
    MainWindow m;
     m.show();
