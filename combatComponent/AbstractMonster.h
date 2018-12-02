4905eabe82a3aa3de7f799e7a590575cb0e16f8c
Kashru <jshenah@connect.ust.hk>
2018-12-02 23:19:08 +0800

    description

--- combatComponent/AbstractMonster.h
+++ combatComponent/AbstractMonster.h
@@ -9,30 +9,34 @@ using std::vector;
 #include "Utils.h"
 #include "FileLoader.h"
 
+//This file is the implementation of the class AbstractMonster.
+//This class is the base class for two key elements in the part of the battle game which are the pet monster and the enemy monster.
+//It contains the common properties and abilities of the two different kinds of monster
+
 class AbstractMonster : public QObject {
     Q_OBJECT
 public:
-    const int ID;
-    const QString NAME;
-    const Type TYPE;
-    const int POSITION;
+    const int ID; //int type, which is used to identify the specific monster object
+    const QString NAME; //QString type, which is the “name” of the specific monster object printed out to the user when gaming
+    const Type TYPE; //Self-defined type “type”, which is used to distinct the different “element property” of different monsters
+    const int POSITION; //int type, which is used to store the position of the monster in the gaming slot (there will be 5 positions for monster to pick, which decides the attack order)
     
-    AbstractMonster(int position, int ID);
-    virtual ~AbstractMonster() = default;
+    AbstractMonster(int position, int ID); //constructor
+    virtual ~AbstractMonster() = default; //destructor
     
-    virtual int attack() = 0;
-    virtual int special_ability() = 0;
+    virtual int attack() = 0; //return int type, which is called when the monster gives attack to others
+    virtual int special_ability() = 0; //return int type, which is called when the monster use special skill
     
-    int get_attack() const;
-    int get_health() const;
-    int get_defense() const;
+    int get_attack() const; //return attack value
+    int get_health() const; //return health value
+    int get_defense() const; //return defense value
 protected:
-    const int ATTACK;
-    const int HEALTH;
-    const int DEFENSE;
+    const int ATTACK; //int type, which is used to store the attack value of a specific monster
+    const int HEALTH; //int type, which is used to store the life value of a specific monster
+    const int DEFENSE; //int type, which is used to store the defense value of a specific monster
     
-    const int COOLDOWN;
-    int turnsCooldown;
+    const int COOLDOWN; //int type, which is used to store the cooldown time of special skill of a specific monster
+    int turnsCooldown; //int type, which is used to calculate the time from the present time to time when the monster can use its special skill
 };
 
 #endif /* AbstractMonster_h */
--- combatComponent/CombatGame.h
+++ combatComponent/CombatGame.h
@@ -11,48 +11,52 @@
 
 class GameWindow; //game window ui
 
+
+//This file is the implementation of the class CombatGame.
+//This class is used to do the battle game by receiving the corresponding value from orb game and return the corresponding signal to the orb game and display part.
+
 class CombatGame : public QObject {
     Q_OBJECT
 public:
-    CombatGame(int level, int petSelection[5]);
+    CombatGame(int level, int petSelection[5]); //constructor
     ~CombatGame(); //some destructor
     
 private:
-    int level;
-    PetMonster* petArray[5];
-    EnemyMonster* enemyArray[5];
+    int level; //int type, which is used to record the current level of the game
+    PetMonster* petArray[5]; //PetMonster* type, which is used to store the pet monsters that used in the current game, the order of the array also stands for the position of the pet monsters
+    EnemyMonster* enemyArray[5]; //EnemyMonster* type, which is used to store the enemy monsters that used in the current game, the order of the array also stands for the position of the enemy monsters
     
-    int turnNumber;
-    int totalHealth;
-    int playerHealth;
-    int playerDefense;
-    int playerTrueDamage;
+    int turnNumber; //int type, which is used to record the turn of the game
+    int totalHealth; //int type, which is calculated by add up all the health value of the pet monster
+    int playerHealth; //int type, which represents the health of the current player
+    int playerDefense; //int type, which represents the defense of the current player
+    int playerTrueDamage; //int type, which stands for the total damage received by the player
 
-    bool gameOver;
+    bool gameOver; //bool type, which states the condition of the game
 
-    void pets_attack(const vector<Combo>& combos);
-    void enemies_attack();
-    int player_recieve_damage(int damage);
+    void pets_attack(const vector<Combo>& combos); //void, which is called when the player finish the orb game and the pet monsters give damage to the enemy
+    void enemies_attack(); //void, which is called when the enemy attack the player
+    int player_recieve_damage(int damage); //int, which is called when the player is attacked, return the current player health
 
-    void ability_attack_enemy(int petPosition, Type TYPE, int damage);
-    void ability_heal_player(int petPosition, int heal);
+    void ability_attack_enemy(int petPosition, Type TYPE, int damage); //void, which is called when a pet monster use its attack skill
+    void ability_heal_player(int petPosition, int heal); //void, which is called when a pet monster use its heal skill
     
 public slots:
-    void start_combat(const vector<Combo>& combos);
-    void activate_pet_ability(int petPosition);
+    void start_combat(const vector<Combo>& combos); //receive when the combat game start
+    void activate_pet_ability(int petPosition); //receive when some pets use their special skill
 
 signals:
-    void pet_attack_enemy(int petPosition, int enemyPosition, int newHealth, bool criticalHit);
-    void enemy_attack_player(int enemyPosition, int newHealth);
-    void player_update_health(int petPosition, int newHealth);
-    void enemy_update_health(int enemyPosition, int newHealth);
-    void special_attack_ready(int petPosition);
-    void pet_ability_animation(bool animationStatus);
-    void enemy_die(int enemyPosition);
-    void player_die();
+    void pet_attack_enemy(int petPosition, int enemyPosition, int newHealth, bool criticalHit); //sent out when a pet attack the enemy
+    void enemy_attack_player(int enemyPosition, int newHealth); //sent out when the enemy attack the player
+    void player_update_health(int petPosition, int newHealth); //sent out when the player health value is changed
+    void enemy_update_health(int enemyPosition, int newHealth); //sent out when the player health value is changed
+    void special_attack_ready(int petPosition); //sent out when a pet’s cooldown turn become 0
+    void pet_ability_animation(bool animationStatus); //sent out when the pet’s special skill is ready
+    void enemy_die(int enemyPosition); //sent out when an enemy is dead
+    void player_die(); //sent out when the player die
     void level_cleared();
-    void combat_end();
-    void combat_text(QString text, bool playerAction);
+    void combat_end(); //sent out when the combat game is finished
+    void combat_text(QString text, bool playerAction); //sent out when a action of the  combat game is taken
 };
 
 #endif /* CombatGame_h */
